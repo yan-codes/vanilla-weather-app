25acd7b4fa73bdf2ce4aa4a5ec1fd14855d87dc4
yan-codes <dlpcardenas@gmail.com>
2022-09-11 17:41:20 +0800

    updated js coordinates

--- src/app.js
+++ src/app.js
@@ -67,7 +67,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
